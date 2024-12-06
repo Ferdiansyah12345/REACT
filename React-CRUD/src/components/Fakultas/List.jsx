38727ccea8ab0a5f894949f4f7cb0e0f9dd50ba6
Ferdiansyah12345 <ferdiansyah_2225110020@mhs.mdp.ac.id>
2024-12-06 15:13:29 +0700

    0

--- React-CRUD/src/components/Fakultas/List.jsx
+++ React-CRUD/src/components/Fakultas/List.jsx
@@ -7,16 +7,34 @@ export default function List() {
 
     useEffect( () => {
         axios 
-        .get("https://project-apiif-3-b.vercel.app/api/fakultas")
+        .get("https://academic-mi5a.vercel.app/api/api/fakultas")
         .then( (response) => {
             console.log(response);
-            setFakultas(response.data.result)
+            setFakultas(response.data.data)
         })
     }, [] )
 
     return (
         <>
             <h2>List Fakultas</h2>
+            <table className="table">
+                <thead>
+                    <tr>
+                        <th>Nama Fakultas</th>
+                        <th>dekan</th>
+                        <th>singkatan</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {fakultas.map( (data) => (
+                        <tr key={data.id}>
+                            <td>{data.nama}</td>
+                            <td>{data.dekan}</td>
+                            <td>{data.singkatan}</td>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
         </>
     )
 }
--- React-CRUD/src/components/Mahasiswa/List.jsx
+++ React-CRUD/src/components/Mahasiswa/List.jsx
@@ -1,7 +1,48 @@
+import React, {useEffect, useState} from "react"
+import axios from 'axios'
+
 export default function List() {
+    // state mahasiswa
+    const [mahasiswa, setMahasiswa] = useState([]);
+
+    useEffect( () => {
+        axios 
+        .get("https://academic-mi5a.vercel.app/api/api/mahasiswa")
+        .then( (response) => {
+            console.log(response);
+            setMahasiswa(response.data.data)
+        })
+    }, [] )
+
     return (
         <>
             <h2>List Mahasiswa</h2>
+            <table className="table">
+                <thead>
+                    <tr>
+                        <th>Nama Mahasiswa</th>
+                        <th>NPM</th>
+                        <th>Email</th>
+                        <th>Hp</th>
+                        <th>alamat</th>
+                        <th>Prodi</th>
+                       
+                    </tr>
+                </thead>
+                <tbody>
+                    {mahasiswa.map( (data) => (
+                        <tr key={data.id}>
+                            <td>{data.nama}</td>
+                            <td>{data.npm}</td>
+                            <td>{data.email}</td>
+                            <td>{data.hp}</td>
+                            <td>{data.alamat}</td>
+                            <td>{data.prodi.nama}</td>
+                            
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
         </>
     )
 }
